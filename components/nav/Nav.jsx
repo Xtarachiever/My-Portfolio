80701a20fb319ef919b48c61aa5400e98a4148f3
Xtarachiever <estheradeyemi796@gmail.com>
2024-01-06 08:21:25 -0800

    Updated the responsiveness and other stylings

--- components/nav/Nav.jsx
+++ components/nav/Nav.jsx
@@ -1,21 +1,39 @@
 "use client";
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.css';
 import { FaLaptopCode } from 'react-icons/fa';
+import { GiHamburgerMenu } from "react-icons/gi";
 import { usePathname, useRouter } from 'next/navigation';
+import { IoMdClose } from "react-icons/io";
 
 const Nav = () => {
+  const [active, setActive] = useState(false);
   const pathName = usePathname();
   return (
     <div className='wrapper'>
         <div>
-          <FaLaptopCode fontSize={'5rem'} className='text-orange'/>
+          <FaLaptopCode fontSize={'4rem'} className='text-orange'/>
         </div>
-        <div className='flex justify-evenly w-[60%] items-center text-white'>
+        {/* Desktop view */}
+        <div className='hidden sm:flex justify-evenly w-[60%] items-center text-white'>
             <Link href="/" className={`${pathName === '/' ? 'text-orange' : ''}`}>About</Link>
             <Link href={"/projects"} className={`${pathName === '/projects' ? 'text-orange' : ''}`}>Projects</Link>
             <Link href={"/contact"} className={`${pathName === '/contact' ? 'text-orange' : ''}`}>Contact Me</Link>
+        </div>
+        {/* Mobile view */}
+        <div className='relative sm:hidden block cursor-pointer'>
+          {
+            !active ?
+            <GiHamburgerMenu fontSize={'2rem'} className='text-orange' onClick={()=>setActive(true)}/> 
+            :
+            <div className='mobile-view'>
+              <IoMdClose className='absolute top-2 right-4' onClick={()=>setActive(false)}/> 
+              <Link href="/" className={`${pathName === '/' ? 'text-orange' : ''}`}>About</Link>
+              <Link href={"/projects"} className={`${pathName === '/projects' ? 'text-orange' : ''}`}>Projects</Link>
+              <Link href={"/contact"} className={`${pathName === '/contact' ? 'text-orange' : ''}`}>Contact Me</Link>
+            </div>
+          }
         </div>
     </div>
   )
